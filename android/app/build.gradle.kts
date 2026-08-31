69e2da22811e5a4eba7d181095ff83a21ca991b3
https://github.com/vinero2026-cpu/Stocks.git <build@vinerox.app>
2026-08-31 16:46:44 +0300

    Raise minimum SDK for Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "app.vinero.vinerox_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
