805ad07acd4665ee0652b58de8f899d83b78d7a1
https://github.com/vinero2026-cpu/Stocks.git <build@vinerox.app>
2026-09-01 12:17:50 +0300

    fix(android): match existing Google Play package

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "app.vinero.vinerox_mobile"
+    namespace = "app.vinero.vinorox_mobile"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "app.vinero.vinerox_mobile"
+            applicationId = "app.vinero.vinorox_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
